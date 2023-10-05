d05dcf00cc1bface738b93f85ddd730bc2e5b9a5
Akubada Daniel <dankubada@gmail.com>
2023-10-05 15:16:09 -0700

    added puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
-#include <stdio.h>
-int main(void) { puts("\"Programming is like building a multilingual puzzle"); return (0); }
-
-#include <stdio.h>
-int main(void) { fputs("Programming is like building a multilingual puzzle\n", stdout); }
+#include<stdio.h>
+/**
+ *  * main - entry point
+ *   *
+ *    * Return: Always 0 (success)
+ *     */
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
