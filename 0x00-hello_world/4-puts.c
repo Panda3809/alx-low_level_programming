8ba70bff99da3eff8a25681a0549057cb1a542a6
Akubada Daniel <dankubada@gmail.com>
2023-10-05 17:01:24 -0700

    added puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
-#include <studio.h>
+#include <stdio.h>
 
 /**
- * 	* main - prints the phrase "programming is like building a multilingual puzzle
- * 	* Return 0: if exited properly, non-zero otgerwise 
- * 	*/
+ *  * main - Entry point
+ *   *
+ *    * Description:
+ *     *   This is the main function that prints a message and returns 0.
+ *      *
+ *       * Return:
+ *       *  Always 0
+ */
 int main(void)
-{
-		puts("\" programming is like building a multilingual puzzle");
-			return(0);
+{	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
