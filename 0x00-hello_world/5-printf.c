c2a33d90a8486e0e70c3b4d1ad6682093aa8492a
Akubada Daniel <dankubada@gmail.com>
2023-10-05 17:54:13 -0700

    added print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *  * main - Entry point of the program
+ *   *
+ *    * Description: This is the main function that uses printf
+ *     *              to print a specific message.
+ *      *
+ *       * Return: Always 0 to indicate successful execution.
+ */
+int main(void)
+{	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
