abb0f22e2491315ef841d338d3469e51928790ce
Akubada Daniel <dankubada@gmail.com>
2023-10-12 19:51:25 -0700

    first debug

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ ** positive_or_negative - Determines if an integer
+ ** @i: The integer to be checked
+ */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	else
+	{
+		printf("%d is zero\n", i);
+	}
+}
