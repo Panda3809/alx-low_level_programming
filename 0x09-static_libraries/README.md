25651542080e78eb84e10676c7e54af1693a03f7
Akubada Daniel <dankubada@gmail.com>
2023-10-30 05:06:52 -0700

    added latest C project

---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+static readme libr file
---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  * main - Entry point of the program
+ *   *
+ *    * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "Hello, world!";
+
+	printf("Length: %d\n", _strlen(str));
+	return (0);
+}
